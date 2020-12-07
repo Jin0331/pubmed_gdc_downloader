6960f7b381886172e4f206f1655a94338c83ee97
Jin0331 <sempre813@hallym.ac.kr>
2020-12-07 17:52:34 +0900

    code update

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 # consumer.subscribe(['test2'])
 # group_id 
-consumer = KafkaConsumer("test2", bootstrap_servers=['localhost:9091','localhost:9092','localhost:9093'],
+consumer = KafkaConsumer("seoul_bike_1_10", bootstrap_servers=['localhost:9091','localhost:9092','localhost:9093'],
                           auto_offset_reset='earliest',group_id='consumer')
 
 for message in consumer:
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,18 +1,43 @@
 from kafka import KafkaProducer
+import requests
 import json
+import time
+
+
+json_url = "http://openapi.seoul.go.kr:8088/4e4258575a73656d32315a46644370/json/bikeList/1/10"
 
 bootstrap_servers = ['localhost:9091', 'localhost:9092', 'localhost:9093']
-topicName = 'test2'
-
-# for json
-# value_serializer= lambda v: v.encode('utf-8')
-# producer.send(topicName, msg.encode("utf-8"))
+topicName = "seoul_bike_1_10"
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-for index in range(1000):
-    future = producer.send(topicName, {index : "hi"})
 
-result = future.get(timeout=120)
+while True:
+    try:
+        response = requests.get(json_url)
+        cnt = 1
+        if response.status_code == 200:
+            print("ok! ", cnt, end=" ")
+            cnt+=1
+            producer.send(topicName, response.json())
+            producer.flush()
+            time.sleep(60)
 
-producer.flush()
+    except Exception as e:
+        print(e)
+        time.sleep(30)          
+
+# # for json
+# # value_serializer= lambda v: v.encode('utf-8')
+# # producer.send(topicName, msg.encode("utf-8"))
+# bootstrap_servers = ['localhost:9091', 'localhost:9092', 'localhost:9093']
+# topicName = 'test2'
+# producer = KafkaProducer(bootstrap_servers = bootstrap_servers,
+#                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+# for index in range(1000):
+#     future = producer.send(topicName, {index : "hi"})
+
+# result = future.get(timeout=120)
+
+# producer.flush()
